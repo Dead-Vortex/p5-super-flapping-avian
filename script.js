ef5a22f167b49c02e3569514e96afc28fdba97b7
Dead-Vortex <milesohalpin@gmail.com>
2026-09-17 17:37:29 +0000

    it works to an extent

--- script.js
+++ script.js
@@ -2,35 +2,60 @@
 
 let avian;
 let tubes = [];
+let scrollSpeed = 5;
+let tubeFrequency = 120;
 
 function setup() {
   createCanvas(windowWidth-100, windowHeight-100);
   background(30);
   avian = new Avian(15, 0.8, -20, 60);
-  tubes.push(new Tube(100, 300, height / 3));
+  tubes.push(new Tube(100, random(250, 400), random(100, height - 500)));
 }
 
 function draw() {
   background(30)
   avian.physicsTick(); // Check for keyboard input and update physics
-  avian.display();    // Draw the ball
+  avian.display();    // Draw the avian
 
-  tubes[0].display();
-  if(tubes[0].pos.x < avian.pos.x && tubes[0].pos.x > 0) {
-    console.log("the thing")
+  for(let tube of tubes) {
+    tube.pos.x -= scrollSpeed;
+    tube.display();
+    if((tube.pos.x < avian.pos.x && tube.pos.x > 0) && (avian.pos.y < tube.gapLocation || avian.pos.y > tube.gapLocation + tube.gapSize) || avian.pos.y > height) {
+      console.log("death")
+      scrollSpeed = 0;
+    }
   }
+
+  if(frameCount % tubeFrequency == 0) {
+    let newGapSize = random(200, 400);
+    let newGapLocation = random(100, height - newGapSize - 100);
+    tubes.push(new Tube(100, newGapSize, newGapLocation));
+
+    if(Math.random() > 0.1) {
+      tubeFrequency--;
+    }
+    if(Math.random() > 0.1) {
+      scrollSpeed+= 0.1;
+    }
+
+    // console.log(newGapSize);
+    // console.log(newGapLocation);
+  }
+
+
+
   if(tubes[0].pos.x < 0) {
     // delete object from array
   }
-  tubes[0].pos.x--;
-  // for(let tube of tubes) {
-  //   tube.display();
-  // }
+
+  // debug display
+  fill(255, 255, 255);
+  text("DEBUG\nAvian Y: " + avian.pos.y + "\nScroll Speed: " + scrollSpeed, 0, 0);
 }
 
 class Avian {
   constructor(jumpHeight, gravity, terminalVelocity, radius) {
-    this.pos = createVector(radius, height / 2);
+    this.pos = createVector(radius, height / 4);
     this.vel = 0;
     this.jumpHeight = jumpHeight;
     this.r = radius;
@@ -61,28 +86,6 @@ class Avian {
     //console.log(this.pos);
   }
 
-  // The "Force" pattern: Force adds to Acceleration
-  // applyForce(force) {
-  //   this.acc.add(force);
-  // }
-
-  // update() {
-  //   // 1. Acceleration changes Velocity
-  //   this.vel.add(this.acc);
-    
-  //   // 2. Limit the speed so it doesn't go infinite
-  //   this.vel.limit(this.topSpeed);
-    
-  //   // 3. Velocity changes Position
-  //   this.pos.add(this.vel);
-    
-  //   // 4. Apply friction (velocity decay)
-  //   this.vel.mult(this.friction);
-    
-  //   // 5. Reset acceleration for the next frame
-  //   this.acc.mult(0);
-  // }
-
   display() {
     fill(255, 150, 0);
     noStroke();
@@ -92,17 +95,15 @@ class Avian {
 
 class Tube {
   constructor(tubeWidth, gapSize, gapLocation) {
-    this.pos = createVector(width / 2, 0);
+    this.pos = createVector(width, 0);
     this.width = tubeWidth;
     this.gapSize = gapSize;
     this.gapLocation = gapLocation;
-    this.topRectHeight = height - this.gapLocation - (this.gapSize / 2);
-    this.bottomRectHeight = height - this.gapLocation + (this.gapSize / 2);
   }
 
   display() {
     fill(0, 255, 0);
-    rect(this.pos.x, 0, this.width, this.topRectHeight);
-    rect(this.pos.x, this.bottomRectHeight, this.width, this.bottomRectHeight);
+    rect(this.pos.x, 0, this.width, this.gapLocation);
+    rect(this.pos.x, this.gapLocation + this.gapSize, this.width, this.gapLocation + this.gapSize);
   }
 }
